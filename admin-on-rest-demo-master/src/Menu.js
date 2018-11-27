c18e4c29a905c4e887bdc1eb1c906f28603fde47
Vishal Patel <vsp20@case.edu>
2018-11-26 19:06:30 -0500

    initial commit

---
+++ admin-on-rest-demo-master/src/Menu.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import SettingsIcon from 'material-ui/svg-icons/action/settings';
+import LabelIcon from 'material-ui/svg-icons/action/label';
+import { translate, DashboardMenuItem, MenuItemLink } from 'admin-on-rest';
+
+import { VisitorIcon } from './visitors';
+import { CommandIcon } from './commands';
+import { ProductIcon } from './products';
+import { CategoryIcon } from './categories';
+import { ReviewIcon } from './reviews';
+
+const items = [
+    { name: 'products', icon: <ProductIcon /> },
+    { name: 'categories', icon: <CategoryIcon /> },
+    
+];
+
+const styles = {
+    main: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        height: '100%',
+    },
+};
+
+const Menu = ({ onMenuTap, translate, logout }) => (
+    <div style={styles.main}>
+        <DashboardMenuItem onClick={onMenuTap} />
+        {items.map(item => (
+            <MenuItemLink
+                key={item.name}
+                to={`/${item.name}`}
+                primaryText={translate(`resources.${item.name}.name`, { smart_count: 2 })}
+                leftIcon={item.icon}
+                onClick={onMenuTap}
+            />
+        ))}
+        <MenuItemLink
+            to="/configuration"
+            primaryText={translate('pos.configuration')}
+            leftIcon={<SettingsIcon />}
+            onClick={onMenuTap}
+        />
+        {logout}
+    </div>
+);
+
+const enhance = compose(
+    connect(state => ({
+        theme: state.theme,
+        locale: state.locale,
+    })),
+    translate,
+);
+
+export default enhance(Menu);
