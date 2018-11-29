8877467d8ad95da40e713c3d65ee5350b1392445
Connor Baumler <connor.baumler@case.edu>
2018-11-29 12:03:32 -0500

    some more queries and stuff

--- connor's (unfinished) stuff/queries.py
+++ connor's (unfinished) stuff/queries.py
@@ -1,16 +1,17 @@
 get_order_id = "select order.id from customer, order, purchased where customer.id={customerID} and order.status={status} and customer.id=purchased.customerID and order.id=purchased.orderid"
 insert_into_order = "insert into order (status) values ({status})"
-insert_into_purchased = "insert into purchased (customer_ID, order_ID) values ({customer_ID},{order_ID})"
-insert_into_includes = "insert into includes (order_ID, product_ID) values ({order}, {productID})"
+insert_into_purchased = "insert into purchased (customerID, orderID) values ({customer_ID},{order_ID})"
+insert_into_includes = "insert into includes (orderID, productID) values ({order}, {productID})"
 get_cart = "select product.name, product.price, product.weight from product, includes where product.ID = includes.product_ID and indludes.order_ID={order}"
 get_free_drones_and_capacities = "select drone.id, drone.capacity from drone where drone.availibility=1"
-get_order_weight = None
-get_product_availability_by_id = None
-get_order_products_and_quantities = None
+get_order_weights = "select product.weight from product, includes where includes.orderID={order} and includes.productID=product.ID"
+get_product_availability_by_id = "select ingredient.amount from contains, ingredient where contains.productID={prod_ID} and contains.ingredientID=ingredient.ID"
+get_order_products = "select productID from includes where orderID={order}"
 get_order_type = None
-set_drone_to_busy = None
-set_order_to_purchased = None
-insert_into_deliveries = None
-remove_product_ingreds_by_prod = None
-update_dest_and_time = None
+set_drone_to_busy = "update drone set drone.status=1 where drone.id={drone_id}"
+set_order_to_purchased = "update order set order.status=1 where order.id={order}"
+insert_into_delivers = "insert into delivers (droneID, orderID) values ({drone_id}, {order})"
+get_product_ingreds_id_and_amount = "select ID, amount from contains where productID={prod_ID}"
+update_ingreds_by_id = "update ingredient set amount={newamount} where ID={ingred_ID}"
+update_dest_and_time = "update order set order.destlong={dest_long}, order.destlat{dest_lat}, order.time={time_to_dest} where order.id={order}"
 get_all_products = "select * from product"
--- connor's (unfinished) stuff/run.py
+++ connor's (unfinished) stuff/run.py
@@ -47,8 +47,8 @@ def sql_execute_return_new_id(sql):
 # uncommenting one of the @app.route decorators.
 
 #@app.route('/')
-def basic_response():
-    return "It works!" #example
+#def basic_response():
+#    return "It works!" #example
 
 #@app.route('/')
 #def template_response():
@@ -82,18 +82,31 @@ def template_response_with_data():
             return "error: checking out without an order"
         else:
             # check if its a valid purchase and stuff
-            order_prods = sql_query(queries.get_order_products_and_quantities)
+            order_prods = sql_query(queries.get_order_products)
+
+            prods = {}
 
             for row in order_prods:
                 prod_id = row[0]
-                prod_quant_wanted = row[1]
-                if prod_quant_wanted < sql_query(queries.get_product_availability_by_id):
-                    return "error: there isn't enough of product " + str(prod_id)
+                if prod_id in prods:
+                    prods[prod_id] += 1
+                else:
+                    prods[prod_id] = 1
+
+            for prod in prods:
+                ingred_avalibilities = sql_execute_return_new_id(queries.get_product_availability_by_id)
+                for ingredrow in ingred_avalibilities:
+                    avalibility = ingredrow[0]
+                    if prods[prod] > avalibility:
+                        return "error: there isn't enough of product id=" + str(prod_id)
             order_type = sql_query(queries.get_order_type)
             if order_type == 0:
                 # Delivery order. Need to deal with drones
                 drones = sql_query(queries.get_free_drones_and_capacities)
-                weight = sql_query(queries.get_order_weight)
+                weights = sql_query(queries.get_order_weights)
+                weight = 0
+                for row in weights:
+                    weight += row[0]
 
                 sel_drone = None
                 for rows in drones:
@@ -106,16 +119,19 @@ def template_response_with_data():
                     return "error: No drones can take this order right now"
                 else:
                     sql_execute(queries.set_drone_to_busy)
-                    sql_execute(queries.insert_into_deliveries)
+                    sql_execute(queries.insert_into_delivers)
                     time_to_dest = geopy.distance.vincenty((conf.store_lat, conf.store_long), (dest_lat, dest_long)).km * conf.drone_speed
                     est_delivery_time = datetime.now() + timedelta(seconds=time_to_dest)
                     sql_execute(queries.update_dest_and_time)
             sql_execute(queries.set_order_to_purchased)
-            for row in order_prods:
-                prod_id = row[0]
-                prod_quant_wanted = row[1]
-                for _ in range(prod_quant_wanted):
-                    sql_execute(queries.remove_product_ingreds_by_prod)
+
+            for prod in prods:
+                ingred_ids_amnts = sql_query(queries.get_product_ingreds_id_and_amount)
+                for row in ingred_ids_amnts:
+                    ingred_id = row[0]
+                    amount = row[1]
+                    newamount = amount - prods[prod]
+                    sql_execute(queries.update_ingreds_by_id)
 
     elif "getallproducts" in request.form:
         return sql_execute(queries.get_all_products)
