92403d015578305e6530cb6f2c2d0b0cada378de
Vishal Patel <vsp20@case.edu>
2018-11-30 09:38:28 -0500

    readme and renamed stuff for submission

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Drinks_and_Drones
-Front end for drink delivery site using drones. Built in React
+### Front end startup instructuions
+To run the front-end locally, navigate to admin-on-rest-demo-master folder and run `npm install` which will install all the necessary files and libraries. 
+
+Once this is done you can simply just run `npm start` and the server should just boot up and defaults to port 3000 on local host. (`localhost:3000`).
+
+Since there were some difficulties in connecting the database, currently there are built in credentials to navigate the site. Using `demo` as both the username and password should gain you entry into the site. 
+
+### Backend Startup instructions
+To run the backend you must use `pip3 install <package-name>` for all the imports necessary in the `run.py` file. Once this is done, you can run the server by using the command `python3 run.py`. The server should run on port number `35230`
--- admin-on-rest-demo-master/src/products/ProductGrid.js
+++ admin-on-rest-demo-master/src/products/ProductGrid.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Item from './Item.js'
 import { Button } from 'react-bootstrap';
 import coffee from './coffee.png';
-
 import axios from 'axios';
+import { propTypes, reduxForm, Field } from 'redux-form';
+
+import TextField from 'material-ui/TextField';
 
 //Here we just have to use item as a component and input as many as we need.
 // also we implement the shopping cart stuff here( not a component )
+
+const styles = {
+    main: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    card: {
+        minWidth: 300,
+    },
+    avatar: {
+        margin: '1em',
+        textAlign: 'center ',
+    },
+    form: {
+        padding: '0 1em 1em 1em',
+    },
+    input: {
+        display: 'flex',
+    },
+    hint: {
+        textAlign: 'center',
+        marginTop: '1em',
+        color: '#ccc',
+    },
+};
+
+
 
 class ProductGrid extends Component{
 	constructor(props){
@@ -15,6 +47,8 @@ class ProductGrid extends Component{
 		this.renderItems = this.renderItems.bind(this);
 		this.state={productList: ""};
 	}
+
+
 	componentDidMount(){
 		// put pull from the shopping cart database here. 
 
@@ -39,6 +73,10 @@ class ProductGrid extends Component{
 		return <ul>{arr.map(item => <Item id={item.id} name={item.name} imgSrc={coffee}/>)}</ul>;
 	}
 
+	handleOrder(){
+		//order backend request here
+	}
+
 	render(){
 		const json = {"coffee":{"name":"coffee","id":"1"},"orange juice":{"name":"orange juice","id":"2"},"strawberry smoothie":{"name":"strawberry smoothie","id":"3"}};
 
@@ -59,6 +97,10 @@ class ProductGrid extends Component{
 					<div className="cartBody">
 						
 					</div>
+				</div>
+				<div className="address">
+					
+					<Button>Place Order</Button>
 				</div>
 			 </div>
 			);
